ba4ef5dd7c6700667f99064e6e90bb7ab7e361b8
Horlando Leão <horlandoleao8@gmail.com>
2020-08-06 22:54:53 -0300

    capitulo 1

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,15 @@
+//obrigatorio
 import React from 'react';
+import './assets/styles/global.css';
+import Landing from './pages/Landing'; 
 
+//funções no react, são funções que retornam html
+//funções são componenetes
+//funções para componentes devem sempre começar com maisculo
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello wor</h1>
-    </div>
+    <Landing/>
   );
 }
 
